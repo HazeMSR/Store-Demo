1e2764a12f2b8f6211132a33bbddd513be9c03e3
Hazel Martinez <hazel@MacBook-Pro-de-Hazel.local>
2020-12-28 03:13:18 -0600

    Heroku deploy

--- nodejs/server.js
+++ nodejs/server.js
@@ -79,10 +79,10 @@ const doUpsert = (req, res, type = 'update') => {
 
 // Production static assets
 if(process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static('../client/build'));
 
 	app.get('*', (req, res) => 
-		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(_dirname, '..', 'client', 'build', 'index.html'))
 	);
 }
 
